dde1f0d8f6190338df6668b85bbafae62bf3bcc4
Johan Svensson <johan.svensson93@outlook.com>
2018-10-12 16:30:24 +0200

    Register code is not working
    Throwing exception

--- Monster/Login/Account.cs
+++ Monster/Login/Account.cs
@@ -6,19 +6,35 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Cryptography;
 
-namespace Monster.Login
+namespace Monster
 {
-    class Account
+    public class Account
     {
-        private string AccountName { get; set; }
-        private string Salt { get; set; }
-        private string PasswordHash { get; set; }
+        private string accountName { get; set; }
+        private string salt { get; set; }
+        private string passwordHash { get; set; }
+        
+        public string AccountName { get => accountName;}
+        public string PasswordHash { get => passwordHash; }
+        public string Salt { get => salt; }
 
         public Account(string accountName, string password)
         {
-            AccountName = accountName;
-            Salt = RandomString(new Random().Next(15, 40));
-            PasswordHash = ComputeSha256Hash(string.Concat(Salt, password));
+            this.accountName = accountName;
+            salt = RandomString(new Random().Next(15, 40));
+            passwordHash = ComputeSha256Hash(string.Concat(Salt, password));
+        }
+
+        public Account(string accountName, string password, string salt)
+        {
+            this.accountName = accountName;
+            this.salt = salt;
+            passwordHash = password;
+        }
+
+        public Account()
+        {
+
         }
 
         private static string RandomString(int length)
@@ -38,7 +54,7 @@ namespace Monster.Login
             }
             return builder.ToString();
         }
-        private static string ComputeSha256Hash(string rawData)
+        public static string ComputeSha256Hash(string rawData)
         {
             // Create a SHA256   
             using (SHA256 sha256Hash = SHA256.Create())
---
+++ Monster/Login/RegisterScreen.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monster
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class RegisterScreen : Window
+    {
+        public RegisterScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Account account = new Account(txtboxusername.Text, txtboxpassword.Password);
+
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LoginDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+
+                string checkDatabaseQuery = $"SELECT COUNT(1) FROM tblUser WHERE Username={account.AccountName}";
+                SqlCommand sqlComm = new SqlCommand(checkDatabaseQuery, sqlCon);
+                sqlComm.CommandType = CommandType.Text;
+                int count = Convert.ToInt32(sqlComm.ExecuteScalar());
+                if (count == 0)
+                {
+                    string query = $"INSERT INTO tblUser [(Username, PasswordHash, Salt)] VALUES [({account.AccountName}, {account.PasswordHash}, {account.Salt})]";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.CommandType = CommandType.Text;
+
+                    LoginScreen loginScreen = new LoginScreen();
+                    loginScreen.Show();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username already exists");
+                }
+              
+              
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
+}
